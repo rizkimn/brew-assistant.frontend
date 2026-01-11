3f74b9f3b92533e181ff9dbb0151e18371cfeee7
rizkimn <rizkimnur0@gmail.com>
2026-01-12 04:05:07 +0900

    first init and first touch of SolidJS

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { Title } from "@solidjs/meta";
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <main>
+      <Title>Hello World</Title>
+    </main>
+  );
+}
